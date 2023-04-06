b6e92b765d9c1d654d41cc53b234c7dfe162aa02
AlandIbrahim <92864566+AlandIbrahim@users.noreply.github.com>
2023-04-06 22:34:35 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿double Trapezo(double[] fx, double h)
+{
+    double sum = 0;
+    for (int i = 1; i < fx.Length - 1; i++) sum += fx[i];
+    return (h / 2) * (fx[0] + 2 * sum+ fx[fx.Length - 1]);
+}
